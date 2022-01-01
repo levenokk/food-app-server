b2ad5328ba794a557d7590dac7928063cab7f980
levenokk <levenokk@gmail.com>
2022-01-01 17:16:46 +0200

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: process.env.NODE_ENV === 'development' ? '.development.env' : '.env',
+      envFilePath:
+        process.env.NODE_ENV === 'development' ? '.development.env' : '.env',
       isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
     }),
     GraphQLModule.forRoot({
       playground: true,
