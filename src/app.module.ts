a2e79d98d254e76d4a70d07ecad3da1899844f7e
levenokk <levenokk@gmail.com>
2022-01-01 17:08:30 +0200

    update gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'development' ? '.development.env' : '.env',
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot({
+      playground: true,
+      debug: true,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
